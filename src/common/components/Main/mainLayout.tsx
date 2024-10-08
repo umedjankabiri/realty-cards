af4cc26f38800b3e29ca15e77db3d197eab13a31
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-08 10:45:46 +0300

    Add Icons and Cards components to MainLayout
    Updated MainLayout to include Icons for sort and filter sections, providing better visual cues. Integrated Cards component to display card content, enhancing the layout's functionality.

--- src/common/components/Main/mainLayout.tsx
+++ src/common/components/Main/mainLayout.tsx
@@ -1,5 +1,7 @@
 import {FC} from 'react';
 import stl from "./mainLayout.module.css"
+import {Icons} from "common/components/Icons/Icons.tsx";
+import {Cards} from "common/components/Cards/Cards.tsx";
 
 export const MainLayout: FC = () => {
 
@@ -11,18 +13,20 @@ export const MainLayout: FC = () => {
                         <h2>Найдено 1600 объявлений</h2>
                     </div>
                     <div className={stl.headerSort}>
-                        <div className={stl.text}>
-                            <span>Сортировка</span>
+                        <div className={stl.sort}>
+                            <Icons name={"sorting"}/> <span>Сортировка</span>
                         </div>
                         <div className={stl.filter}>
-                            <span>По дате: от самых новых</span>
+                            <span>По дате: от самых новых</span> <Icons name={"arrow"}/>
                         </div>
                     </div>
                 </div>
                 <div className={stl.cards}>
+                    <Cards />
+                </div>
+                <div className={stl.pagination}>
 
                 </div>
-                <div className={stl.pagination}></div>
             </div>
         </div>
     );
