917b0d2fd192dae3cbeb02757c184e870f4e4efe
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-08 04:24:23 +0300

    Create Cards component
    Add a new Cards component to the project structure. The component currently renders an empty div with a CSS module class. This sets up the initial structure for future enhancements.

---
+++ src/common/components/Cards/cards.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+import stl from "./cards.module.css";
+
+export const Cards: FC = () => {
+
+    return (
+        <div className={stl.cardsWrapper}>
+
+        </div>
+    );
+};
