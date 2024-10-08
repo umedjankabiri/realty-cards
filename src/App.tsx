945122b52354c93349bde5938c557db569e393ed
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-08 10:48:12 +0300

    Add MainLayout component to App
    This commit updates the App component to include the MainLayout component from 'common/components/Main/mainLayout'. This change enhances the application's structure by introducing a consistent layout across the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
+import { MainLayout } from 'common/components/Main/mainLayout';
+import './App.css';
 
 function App() {
 
   return (
-    <>
-        
-    </>
+    <div  className={"App"}>
+        <MainLayout />
+    </div>
   )
 }
 
