42eb97ae69f7fdc263a53d5b3fa39167fb841de0
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-08 10:47:30 +0300

    Add card data utility with structured card information
    This commit introduces a new utility module `cardData.ts` that provides structured information for various real estate properties in Turkey. The data includes detailed attributes such as images, descriptions, addresses, property IDs, sizes, and prices for accurate representation.

---
+++ src/common/utils/cardData.ts
@@ -0,0 +1,97 @@
+import {CardProps} from "common/types/Card/CardProps.ts";
+
+export const cardData1: CardProps[] = [
+    {
+        image: {url: "./src/assets/images/image1.png", alt: "image1"},
+        description: "Квартира 3+1 в Мерсине, Турция, 94 м²",
+        address: "Турция, Мерсин, район Тедже",
+        ID: "AZ73310",
+        square: "94 м²",
+        rooms: "3+1",
+        nearSea: "450 м",
+        price: "€ 91200"
+    },
+    {
+        image: {url: "./src/assets/images/image2.png", alt: "image2"},
+        description: "Квартира 2+1 в Мерсине, Турция, 100 м²",
+        address: "Турция, Мерсин",
+        ID: "AZ73351",
+        square: "100 м²",
+        rooms: "2+1",
+        nearSea: "450 м",
+        price: "€ 100 000"
+    },
+    {
+        image: {url: "./src/assets/images/image3.png", alt: "image3"},
+        description: "Квартира 4+1 в Стамбуле, Турция, 395 м²",
+        address: "Турция, Стамбул, район Шишли",
+        ID: "AZ74684",
+        square: "395 м²",
+        rooms: "4+1",
+        nearSea: "2100 м",
+        price: "€ 5 202 000"
+    },
+]
+export const cardData2: CardProps[] = [
+    {
+        image: {url: "./src/assets/images/image4.png", alt: "image4"},
+        description: "Квартира 1+1 в Стамбуле, Турция, 62 м²",
+        address: "Турция, Стамбул, район Шишли",
+        ID: "AZ74924",
+        square: "62 м²",
+        rooms: "1+1",
+        nearSea: "4300 м",
+        price: "€ 480 000"
+    },
+    {
+        image: {url: "./src/assets/images/image5.png", alt: "image5"},
+        description: "Квартира 3+1 в Стамбуле, Турция, 104 м²",
+        address: "Турция, Стамбул, район Бахчелиэвлер",
+        ID: "AZ74613",
+        square: "104 м²",
+        rooms: "3+1",
+        nearSea: "5400 м",
+        price: "€ 1 219 000"
+    },
+    {
+        image: {url: "./src/assets/images/image6.png", alt: "image6"},
+        description: "Квартира 1+1 в Стамбуле, Турция, 62 м²",
+        address: "Турция, Стамбул, район Ускюдар",
+        ID: "AZ74684",
+        square: "395.45 м²",
+        rooms: "4+1",
+        nearSea: "2100 м",
+        price: "€ 491 800"
+    },
+]
+export const cardData3: CardProps[] = [
+    {
+        image: {url: "./src/assets/images/image7.png", alt: "image7"},
+        description: "Квартира 2+1 в Стамбуле, Турция, 126²",
+        address: "Турция, Стамбул",
+        ID: "AZ74727",
+        square: "126 м²",
+        rooms: "2+1",
+        price: "€ 446 200"
+    },
+    {
+        image: {url: "./src/assets/images/image8.png", alt: "image8"},
+        description: "Квартира 2+1 в Стамбуле, Турция, 128 м²",
+        address: "Турция, Стамбул, район Сарыер",
+        ID: "AZ74613",
+        square: "128 м²",
+        rooms: "2+1",
+        nearSea: "6000 м",
+        price: "€ 863 300"
+    },
+    {
+        image: {url: "./src/assets/images/image9.png", alt: "image9"},
+        description: "Квартира 1+1 в Стамбуле, Турция, 102 м²",
+        address: "Турция, Стамбул, район Ускюдар",
+        ID: "AZ74706",
+        square: "102 м²",
+        rooms: "1+1",
+        nearSea: "3000 м",
+        price: "€ 519 000"
+    },
+]
