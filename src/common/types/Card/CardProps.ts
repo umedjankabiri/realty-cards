c6f870eef570c2ca3a2eee13b584271e8c9d6c1f
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-08 10:47:20 +0300

    Add CardProps type to Card component
    Introduce a new CardProps type definition for the Card component to specify the expected properties. This includes fields for image details, description, address, ID, square footage, number of rooms, optional proximity to the sea, and price.

---
+++ src/common/types/Card/CardProps.ts
@@ -0,0 +1,13 @@
+export type CardProps = {
+    image: {
+        url: string;
+        alt: string;
+    }
+    description: string
+    address: string
+    ID: string
+    square: string
+    rooms: string
+    nearSea?: string
+    price: string
+}
